5d541b90f3ed1a9dc3c4aff7c90fa133b6cd986b
Albin Holmberg <albinholmberg@icloud.com>
2026-04-08 10:38:55 +0200

    Extract Canasta game engine from UI

---
+++ src/lib/canastaTesting.js
@@ -0,0 +1,12 @@
+export {
+  makeGame,
+  drawTwoState,
+  canTakeDiscard,
+  takeDiscardStackState,
+  pickBotMeldCardIds,
+  discardState,
+  applyMeld,
+  applyMeldMany,
+  computeRoundResults,
+  updateTotalsAfterRound,
+} from "./canastaEngine";
